c3f3287c187a16d9bd08ac0e4eb58a2418279023
Ali Gholipour <164420321+MrAz0000@users.noreply.github.com>
2025-07-21 12:08:04 +0330

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,21 @@
+//in before builder builds
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateIssuerSigningKey = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ClockSkew = TimeSpan.Zero,
+        ValidIssuer = builder.Configuration["JwtSettings:Issuer"],
+        ValidAudience = builder.Configuration["JwtSettings:Audince"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.
+            GetBytes(builder.Configuration["JwtSettings:Key"]!))
+    };
+});
